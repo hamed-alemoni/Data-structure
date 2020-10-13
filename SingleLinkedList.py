ce6bf554bca1011e3fe4d2b9c63b3b53ceb2eb77
HappyDead <hamed.alemonie@gmail.com>
2020-10-13 20:27:49 +0330

    now single link list is ready to use

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -3,18 +3,48 @@ from Node import Node
 
 class SingleLinkedList:
     def __init__(self):
-        self.__head = None
-        self.__tail = None
+        self.__head: Node = None
+        self.__tail: Node = None
         self.__size = 0
 
     # this method check is list empty or not
     def is_empty(self):
         return self.__size == 0
 
-    # this method add a new node as a new head
+    # this method returns size of the list
+    def size(self):
+        return self.__size
+
+    # this method add a new node as a new head to the list
     def add_first(self, element):
         self.__head = Node(element, self.__head)
         if self.is_empty():
             self.__tail = self.__head
 
         self.__size = self.__size + 1
+
+    # this method add a new node as a new tail to the list
+    def add_last(self, element):
+        new_node = Node(element, None)
+        if self.is_empty():
+            self.__head = new_node
+            self.__tail = self.__head
+        else:
+            self.__tail.next = new_node
+            self.__tail = new_node
+
+        self.__size = self.__size + 1
+
+    # this method remove the head of the list
+    def remove_first(self):
+        if self.is_empty():
+            return None
+        new_head = self.__head.next
+
+        self.__head = new_head
+
+        self.__size = self.__size - 1
+
+        if self.is_empty():
+            self.__tail = None
+
