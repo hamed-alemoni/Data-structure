ddad33f03787be395af8e18e3c06893a52926e5c
HappyDead <hamed.alemonie@gmail.com>
2020-10-13 18:35:42 +0330

    built node class for our single linked list

--- Node.py
+++ Node.py
@@ -1,2 +1,20 @@
-#class Node:
-    #def __init__(self):
+class Node :
+    def __init__(self,element,next):
+        self.__element = element
+        self.__next : Node = next
+
+    @property
+    def element(self):
+        return self.__element
+
+    @element.setter
+    def element(self,element):
+        self.__element = element
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self,next):
+        self.__next = next
