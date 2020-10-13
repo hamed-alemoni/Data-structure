edfb2b1b17b0b697cd9ec7593d9af8ed57990d3d
HappyDead <hamed.alemonie@gmail.com>
2020-10-13 18:36:58 +0330

    add_first method and is_empty method to single linked list class

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -1,1 +1,20 @@
-# first linked list
+from Node import Node
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
+
+    # this method check is list empty or not
+    def is_empty(self):
+        return self.__size == 0
+
+    # this method add a new node as a new head
+    def add_first(self, element):
+        self.__head = Node(element, self.__head)
+        if self.is_empty():
+            self.__tail = self.__head
+
+        self.__size = self.__size + 1
