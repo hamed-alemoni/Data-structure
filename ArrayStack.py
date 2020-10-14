e272d0a0b581dee4608e64e4ff87c196be3b09fb
HappyDead <hamed.alemonie@gmail.com>
2020-10-14 18:27:00 +0330

    built another data structure as Array Stack that means a Stack which was wrote by array

---
+++ ArrayStack.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class ArrayStack(object):
+
+    def __init__(self, capacity=10000):
+        self.__t = -1
+        self.__data = np.empty(capacity)
+
+    # this method show size of stack
+    def size(self):
+        return self.__t + 1
+
+    # this method determines the stack is empty or not
+    def is_empty(self):
+        return self.__t == -1
+
+    # this method pushes an element upon of the stack
+    def push(self, element):
+        if self.__t == self.__data.size:
+            raise OverflowError('Stack is full')
+        self.__t = self.__t + 1
+        self.__data[self.__t] = element
+
+    # this method removes the upon element of the stack
+    def pop(self):
+        if self.is_empty():
+            return None
+        self.__data[self.__t] = None
+        self.__t = self.__t - 1
