9713ef7cc8facf6c797ac535adb21e2881ffdb3b
HappyDead <hamed.alemonie@gmail.com>
2020-10-14 18:41:48 +0330

    made this class for use less code and the other classes can inheritance from it

---
+++ Stack.py
@@ -0,0 +1,20 @@
+class Stack(object):
+
+    # this method show size of stack
+    def size(self):
+        pass
+
+    # this method determines the stack is empty or not
+    def is_empty(self):
+        pass
+
+    # this method pushes an element upon of the stack
+    def push(self, element):
+        pass
+
+    # this method removes the upon element of the stack
+    def pop(self):
+        pass
+
+    def top(self):
+        pass
